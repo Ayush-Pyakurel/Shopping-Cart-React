21264f5338321127f793cb7cca24657812012cf3
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2022-06-26 22:38:35 +0545

    Navbar is created

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@
 import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
-//component import from Page folder
+//component import from Page/Components folder
 import { Home } from './Pages/Home';
 import { Store } from './Pages/Store';
 import { About } from './Pages/About';
+import { Navbar } from './Components/Navbar';
 
 function App(): JSX.Element {
   return (
-    <Container>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
